00ad2ddead91a627acc1003bd4d3832356c2f96b
Rin <yukih4ru@gmail.com>
2021-06-13 22:12:15 +0800

    Update Main.py

--- 01/Main.py
+++ 01/Main.py
@@ -55,3 +55,17 @@ data_bool = False
 print("data : ", data_bool)
 print("- bertipe : ", type(data_bool))
 
+#Tipe data Khusus
+
+#Bilangan kompleks
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("- bertipe : ", type (data_complex))
+
+#tipe data dari bahasa C
+
+from ctypes import c_double
+
+data_c_double = c_double(10.5)
+print("data : ", data_c_double)
+print("- bertipe : ", type (data_c_double))
