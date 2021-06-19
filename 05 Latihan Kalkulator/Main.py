ee82b062ae8e6c154a83fbdfb19de1c8825177f3
Rin <yukih4ru@gmail.com>
2021-06-19 09:36:27 +0800

    Create Main.py

---
+++ 05 Latihan Kalkulator/Main.py
@@ -0,0 +1,15 @@
+"""
+Author : Rin
+Discord : Rin#5535
+Date : 13/06/2021
+"""
+
+# Kalkulator dengan looping
+
+print("""Kalkulator
+1. Penambahan
+2. Pengurangan
+3. Perkalian
+4. Pembagian
+5. 
+6. """)
